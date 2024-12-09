6c677cdc5eeff6136e2a1dd2a728545ec5e669f9
aethlios <t.chambaretaud@yeswehack.com>
2024-12-09 20:29:34 +0100

    v1.3.4

--- src/resetTolkien/__init__.py
+++ src/resetTolkien/__init__.py
@@ -1,7 +1,7 @@
 # __init__.py
 
-__version__ = version = "1.3.3"
-__version_tuple__ = version_tuple = (1, 3, 3)
+__version__ = version = "1.3.4"
+__version_tuple__ = version_tuple = (1, 3, 4)
 __copyright__ = "Copyright (c) 2024 Tom CHAMBARETAUD"
 
 from resetTolkien.cli import main
